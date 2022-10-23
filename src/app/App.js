a58e2d414d434d6a61f1d14d03e2751eac4d6ef5
Dmitriy0642 <jesusdontdie@gmail.com>
2022-10-23 22:53:41 +0300

    ready-to-task-project

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Users from "./components/users";
+import { Switch, Route } from "react-router-dom";
+import NavBar from "./components/navBar";
+import MainPage from "./components/mainPage";
+import Login from "./components/login";
 
 function App() {
     return (
-       <Users/>
+        <div>
+            <NavBar/>
+            <Switch>
+                <Route path="/users/:userId?" component={Users}/>
+                <Route path="/main" component={MainPage}/>
+                <Route path="/login" component={Login}/>
+            </Switch>
+        </div>
         );
 }
 
